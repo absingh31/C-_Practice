1db8b38edb60999d0f6f3cdc12d9501fd58fb9c9
absingh31 <absinghemail@gmail.com>
2017-12-22 15:12:09 +0530

    sm

---
+++ cpp_basics.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<stdlib.h>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+int power_c(int x, int y)
+{
+	int result=1;
+	for (int i = 0; i < y; ++i)
+	{
+		result = result * x;
+	}
+	return result;
+}
+
+void hello(char *name)
+{
+	cout<<"Hello "<<name	<<endl;	
+}
+
+
+int main()
+{
+	int x, y;
+	char name[100];
+
+	string s;
+	cin>>s;
+	reverse(s.begin(), s.end());
+	cout<<s<<endl;
+
+	// int *a, n, num, index;
+	// cin>>n>>num>>index;
+	// a = (int*)calloc(n+1, sizeof(int));
+	// cout<<"Enter the elements"<<endl;
+	// for (int i = 0; i < n; ++i)
+	// 	cin>>a[i];
+	// for(int i = n; i>=index; i--)
+	// 	a[i] = a[i-1];
+	// *(a+index) = num;
+	// for (int i = 0; i <= n; ++i)
+	// 	cout<<a[i]<<endl;
+	// free(a);
+
+	// cout<<"Enter your name "<<endl;
+	// cin>>name;
+	// hello(name);
+
+	// cout<<"Enter the base and power values";
+	// cin>>x>>y;
+	// cout<<"Value is "<<power_c(x,y);
+
+	return 0;
+}
--- stl_try.cpp
+++ stl_try.cpp
@@ -4,8 +4,9 @@ using namespace std;
 int main()
 {
 	string str1,str2;
-	str1="abhishek";
-	str2 = "singh";
+	cin>>str1>>str2;
+	// str1="abhishek";
+	// str2 = "singh";
 	int a=4,b=6;
 	cout<<str1<<str2<<endl;
 	cout<<str1.capacity()<<endl;
