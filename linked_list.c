cbc5a66d42d7856738270fb5ebbd58c1e292b3b1
absingh31 <absinghemail@gmail.com>
2017-12-26 15:04:17 +0530

    lls

--- linked_list.c
+++ linked_list.c
@@ -17,6 +17,7 @@ void traversal(struct Node *n)
 		i++;
 		n = n->next;
 	}
+	i=0;
 }
 
 void push_head(struct Node **head_ref, int new_data)
@@ -90,6 +91,50 @@ void delete_node(struct Node **head_ref, int position)
 	return;
 }
 
+int length_iter(struct Node **head_ref)
+{
+	int count=0;
+	struct Node *n = (struct Node*)malloc(sizeof(struct Node));
+	n = *head_ref;
+
+	while(n != NULL)
+	{
+		n = n->next;
+		count++;
+	}
+
+	return count;
+}
+
+int length_rec(struct Node *head_ref)
+{
+	if(head_ref == NULL)
+		return 0;
+
+	return 1 + length_rec(head_ref->next);
+}
+
+void swap_nodes(struct Node **head_ref, int data1, int data2)
+{
+	struct Node *pos1 = (struct Node*)malloc(sizeof(struct Node));
+	pos1 = *head_ref;
+	struct Node *pos2 = (struct Node*)malloc(sizeof(struct Node));
+	pos2 = *head_ref;
+
+	while(pos1->data!=data1)
+		pos1 = pos1->next;
+
+	while(pos2->data!=data2)
+		pos2 = pos2->next;
+
+	printf("THE DATA IS %d\n", pos1->data);
+	pos1->data = pos1->data + pos2->data;
+	pos2->data = pos1->data - pos2->data;
+	pos1->data = pos1->data - pos2->data;
+
+	return;
+}
+
 int main()
 {
 	struct Node* head = NULL;
@@ -112,13 +157,18 @@ int main()
 	push_head(&head, 100);
 	append(&head, 200);
 	insert_after(head->next, 300);
-	delete_node(&head, 7);
+//	delete_node(&head, 5);
 	traversal(head);
+	swap_nodes(&head, 100, 200);
+	traversal(head);
+
+	int len_iter = length_iter(&head);
+	int len = length_rec(head);
+	printf("Length of linked list is %d or %d\n", len, len_iter);
 
 	free(head);
 	free(second);
 	free(third);
-	
 
 	return 0;
 } 
