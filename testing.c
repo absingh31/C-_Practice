9fae910a4b5bda61740d47678cdf93b588dd40c9
absingh31 <absinghemail@gmail.com>
2018-01-08 13:21:43 +0530

    test

--- testing.c
+++ testing.c
@@ -1,55 +1,64 @@
-#include<stdio.h>
-#include<stdlib.h>
-// void haha(int *d)
+// #include<stdio.h>
+// #include<stdlib.h>
+// // void haha(int *d)
+// // {
+// // 	*d=12;
+// // }
+
+// int* pon(int *a)
 // {
-// 	*d=12;
+// 	int *b;
+// 	a=b;
+// 	return a;
 // }
 
-int* pon(int *a)
-{
-	int *b;
-	a=b;
-	return a;
-}
+// int main()
+// {
+// 	int *a;
+
+// 	a = (int*)malloc(sizeof(int));
+// 	*a = 12;
+// 	// a=0;
+// 	// haha(&a);
+// 	// *a=12;
+// 	printf("%p\n", a);
+// 	// printf("%d\n", *a);
+// 	// *a=12;
+// 	// printf("%d\n", *a);
+// 	 pon(a);
+// 	 printf("%p\n", a);
+
+// 	// char a, b[] = "abhishek";
+// 	// a = getchar();
+// 	// printf("%c\n", a);
+// 	// printf("%s\n", b);
+
+// 	return 0;
+// }
+
+// // I’m writing to you regarding the Internship role that opened up recently. I came across the job description on LinkedIn, and was delighted to find that my academic accomplishments meet all of the necessary requirements. I am seeking a challenging but rewarding internship, which is why I was drawn to this exciting opportunity.
+
+// // As a Computer Science student at the Vellore Institute of Technology, I have acquired skills in Networks, Operating Systems, Managing team, and Cyber Security. Currently I hold a 3.48 GPA and have been on the best project awards list every semester. While in the college of Computer Science I have strategically focused my coursework in the following areas:
+
+// //     Networking
+// //     Operating Systems
+// //     Cyber Security
+// //     Data structure and Algorithms
+// //     Software Engineering
+
+// // Using my knowledge of the above, I designed lot of software for college and the company I've Interned for. The projects were so well received that I was awarded best project award in my college.
+
+// // I would be delighted to have an opportunity to personally interview with you. Please accept the enclosed resume and feel free to contact me at your earliest convenience.  I appreciate your time and consideration.
+
+// // Yours sincerely,
+
+// // Abhishek Singh
+
+#include<stdio.h>
 
 int main()
 {
-	int *a;
-
-	a = (int*)malloc(sizeof(int));
-	*a = 12;
-	// a=0;
-	// haha(&a);
-	// *a=12;
-	printf("%p\n", a);
-	// printf("%d\n", *a);
-	// *a=12;
-	// printf("%d\n", *a);
-	 pon(a);
-	 printf("%p\n", a);
-
-	// char a, b[] = "abhishek";
-	// a = getchar();
-	// printf("%c\n", a);
-	// printf("%s\n", b);
+	char *a;
 
 	return 0;
 }
-
-I’m writing to you regarding the Internship role that opened up recently. I came across the job description on LinkedIn, and was delighted to find that my academic accomplishments meet all of the necessary requirements. I am seeking a challenging but rewarding internship, which is why I was drawn to this exciting opportunity.
-
-As a Computer Science student at the Vellore Institute of Technology, I have acquired skills in Networks, Operating Systems, Managing team, and Cyber Security. Currently I hold a 3.48 GPA and have been on the best project awards list every semester. While in the college of Computer Science I have strategically focused my coursework in the following areas:
-
-    Networking
-    Operating Systems
-    Cyber Security
-    Data structure and Algorithms
-    Software Engineering
-
-Using my knowledge of the above, I designed lot of software for college and the company I've Interned for. The projects were so well received that I was awarded best project award in my college.
-
-I would be delighted to have an opportunity to personally interview with you. Please accept the enclosed resume and feel free to contact me at your earliest convenience.  I appreciate your time and consideration.
-
-Yours sincerely,
-
-Abhishek Singh
