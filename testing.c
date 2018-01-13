8f4d85b336f618aec7f5a50a9e0384c3d09a2f9a
absingh31 <absinghemail@gmail.com>
2018-01-13 15:15:26 +0530

    mist

--- testing.c
+++ testing.c
@@ -36,38 +36,34 @@
 // 	return 0;
 // }
 
-// // I’m writing to you regarding the Internship role that opened up recently. I came across the job description on LinkedIn, and was delighted to find that my academic accomplishments meet all of the necessary requirements. I am seeking a challenging but rewarding internship, which is why I was drawn to this exciting opportunity.
+// I’m writing to you regarding the Internship role that opened up recently. I came across the job description on LinkedIn, and was delighted to find that my academic accomplishments meet all of the necessary requirements. I am seeking a challenging but rewarding internship, which is why I was drawn to this exciting opportunity.
 
-// // As a Computer Science student at the Vellore Institute of Technology, I have acquired skills in Networks, Operating Systems, Managing team, and Cyber Security. Currently I hold a 3.48 GPA and have been on the best project awards list every semester. While in the college of Computer Science I have strategically focused my coursework in the following areas:
+// As a Computer Science student at the Vellore Institute of Technology, I have acquired skills in Networks, Operating Systems, Managing team, and Cyber Security. Currently I hold a 3.48 GPA and have been on the best project awards list every semester. While in the college of Computer Science I have strategically focused my coursework in the following areas:
 
-// //     Networking
-// //     Operating Systems
-// //     Cyber Security
-// //     Data structure and Algorithms
-// //     Software Engineering
+//     Networking
+//     Operating Systems
+//     Cyber Security
+//     Data structure and Algorithms
+//     Software Engineering
 
-// // Using my knowledge of the above, I designed lot of software for college and the company I've Interned for. The projects were so well received that I was awarded best project award in my college.
+// Using my knowledge of the above, I designed lot of software for college and the company I've Interned for. The projects were so well received that I was awarded best project award in my college.
 
-// // I would be delighted to have an opportunity to personally interview with you. Please accept the enclosed resume and feel free to contact me at your earliest convenience.  I appreciate your time and consideration.
+// I would be delighted to have an opportunity to personally interview with you. Please accept the enclosed resume and feel free to contact me at your earliest convenience.  I appreciate your time and consideration.
 
-// // Yours sincerely,
+// Yours sincerely,
 
-// // Abhishek Singh
+// Abhishek Singh
 
 #include<stdio.h>
-void swap(char **str1, char **str2)
-{
-  char *temp = *str1;
-  *str1 = *str2;
-  *str2 = temp;
-}  
-  
+ 
 int main()
 {
-  char *str1 = "geeks";
-  char *str2 = "forgeeks";
-  swap(&str1, &str2);
-  printf("str1 is %s, str2 is %s", str1, str2);
-  getchar();
-  return 0;
+	int a=100, b=200, c=300;
+	if(!a>=100)
+	{
+		b=300;
+		c=400;
+	}
+	printf("%d %d %d %d\n", a,b,c,(!a));
+	return 0;
 }
