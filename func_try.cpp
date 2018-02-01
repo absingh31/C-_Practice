9ca33d3d0eaf1035dd015eb16990c989d3d14929
absingh31 <absinghemail@gmail.com>
2018-02-01 20:43:22 +0530

    more

--- func_try.cpp
+++ func_try.cpp
@@ -4,18 +4,25 @@ using namespace std;
 
 int x;
 
-bool ise()
-{
-	if(x==0)
-		return true;
-	else
-		return false;
-}
+// bool ise()
+// {
+// 	if(x==0)
+// 		return true;
+// 	else
+// 		return false;
+// }
+
+
 
 int main()
 {
-	x++;
-	bool a=ise();
-	cout<<a<<endl;
+	// x++;
+	// bool a=ise();
+	// cout<<a<<endl;
+
+	double a=19.00;
+	int b= (int)a;
+	if(a-b==0.0)
+		cout<<b<<endl;
 	return 0;
 }
