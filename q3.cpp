64d1eff2b6ba5783fe06c06fa4098780c9e782c7
absingh31 <absinghemail@gmail.com>
2018-03-28 21:04:57 +0530

    problem statement given in file

---
+++ q3.cpp
@@ -0,0 +1,29 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////Q : Find the no of odd number of factors in a given number./////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int num;
+	int count = 0;
+
+	cout<<"Enter the number"<<endl;
+	cin>>num;
+
+	// TRAVERSE TILL HALF OF THE NUMBER
+	for (int i = 1; i < num/2; ++i)
+		if(num%i == 0 && num%2 != 0)
+			count++;
+
+	// THIS IS DONE IF NUMBER IS ODD THEN NUMBER ITSELF IS ODD FACTOR BUT THIS IS NOT TRUE FOR EVEN
+	if(num%2 != 0)
+		count++;
+
+	cout<<"The number of odd factors of the given number are : "<<count<<endl;
+
+	return 0;
+}
