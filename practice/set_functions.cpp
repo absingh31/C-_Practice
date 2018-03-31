065287abd2a493c515fcbe7dc45b6ea55759522b
absingh31 <absinghemail@gmail.com>
2018-03-31 18:48:40 +0530

    useful set utilities

---
+++ practice/set_functions.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <set>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+// SET IS JUST LIKE BINARY SEARCH TREE
+
+void cool_print(set<int> s1)
+{
+	for(auto &val:s1)
+		cout<<val<<endl;
+}
+
+int main()
+{
+	list<int> l1({1,2,3,4,2,3,4});
+
+	//way of initialising a set, also this could be done using the above method in which list is initialised
+	set<int> s1(l1.begin(), l1.end());
+
+	// insert the element in the set
+	s1.insert(7);
+
+	// returns 1 if n is present and 0 otherwise
+	cout<<s1.count(10)<<endl;
+
+	// return the iterator to element to be found or s1.end() otherwise
+	cout<<*(s1.find(3))<<endl;
+	//.begin(), .end(), .rbegin(), .rend(), .empty(), .clear()(deletes the whole set), .erase(), 	 
+
+	cool_print(s1);	
+
+	return 0;
+}
